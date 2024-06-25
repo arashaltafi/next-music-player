484d4f106b486cdf3e7448bed9f710ee45e879a6
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-25 23:51:38 +0430

    add meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="fa" dir="rtl">
+    <Html lang="fa" dir="rtl" className="scroll-smooth">
       <Head>
         <meta name="darkreader-lock" />
+        <meta name="HandheldFriendly" content="true" />
         <link rel="icon" href="/images/logo.png" />
         <meta charSet="utf-8" />
         <meta name="google" content="notranslate" />
