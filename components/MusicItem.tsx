7fe10dff174851f7012ef7fc9c51a851b52cafd0
Arash Altafi <arashaltafi1377@gmail.com>
2024-06-25 19:14:07 +0430

    page music more

--- components/MusicItem.tsx
+++ components/MusicItem.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import React from 'react'
+import { IoIosArrowBack } from "react-icons/io";
 
 interface PropsType {
     id: number,
@@ -6,15 +8,48 @@ interface PropsType {
     singer: string,
     image: string,
     path: string
+    text: string
 }
 
 const MusicItem = (props: PropsType) => {
     return (
-        <div
-            data-key={props.name}
-            className='flex flex-col items-center justify-center'
-        >
-            {props.name}
+        <div className='w-full flex flex-col gap-4 items-center justify-between px-8 py-4 rounded-xl bg-slate-600 shadow-boxShadow'>
+            <div className='mt-6 mb-8 w-full flex gap-16 items-center justify-between'>
+                <div className='w-full h-full flex flex-col gap-4 items-stretch justify-between'>
+                    <h5 className='text-lg'>موزیک: <span className='text-xl'>{props.name}</span></h5>
+                    <h5 className='text-lg'>خواننده: <span className='text-xl'>{props.singer}</span></h5>
+                    <p className='text-lg line-clamp-3 text-justify'>متن: <span className='text-base'>{props.text}</span></p>
+
+                    <button
+                        className='btn btn-green'
+                        data-download={props.name}
+                    >
+                        دانلود موزیک
+                    </button>
+                    <button
+                        className='btn btn-sky'
+                        data-play={props.name}
+                    >
+                        پخش موزیک
+                    </button>
+                </div>
+                <Image
+                    className='w-1/3 h-auto rounded-boxRadius border border-dashed border-rose-500 shadow-musicShadow'
+                    src={props.image}
+                    alt={props.name}
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div className='my-4 w-full flex items-center justify-end gap-8'>
+                <button
+                    className='flex gap-2 items-center justify-center btn btn-sky-outline'
+                    data-click={props.name}
+                >
+                    ادامه مطلب
+                    <IoIosArrowBack />
+                </button>
+            </div>
         </div>
     )
 }
--- pages/musics/[category].tsx
+++ pages/musics/[category].tsx
@@ -10,13 +10,22 @@ const Musics = ({ data, category }: { data: any, category: string }) => {
 
     const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         const target = e.target as HTMLDivElement
-        const targetDiv = target.closest('div')
-        if (!targetDiv) return
+        const targetBtn = target.closest('button')
+        if (!targetBtn) return
 
-        const key = targetDiv.getAttribute('data-key')
-        if (!key) return
+        const click = targetBtn.getAttribute('data-click')
+        const download = targetBtn.getAttribute('data-download')
+        const play = targetBtn.getAttribute('data-play')
 
-        router.push(RoutesAddress.MUSIC + "/" + key.replaceAll(' ', '-'))
+        if (click) {
+            router.push(RoutesAddress.MUSIC + "/" + click.replaceAll(' ', '-'))
+        } else if (download) {
+
+        } else if (play) {
+
+        } else {
+            return
+        }
     }
 
     return (
@@ -25,7 +34,7 @@ const Musics = ({ data, category }: { data: any, category: string }) => {
                 <title>{category} | موزیک آنلاین</title>
                 <meta name="description" content={`${category} موزیک آنلاین`} />
             </Head>
-            <div onClick={(e) => handleClick(e)} className='mt-10 w-full flex flex-col gap-8 items-center justify-start px-8'>
+            <div onClick={(e) => handleClick(e)} className='mt-10 w-full flex flex-col gap-12 items-center justify-start px-8'>
                 <h2 className='self-start font-bold text-4xl'>{category}:</h2>
 
                 {
@@ -36,6 +45,7 @@ const Musics = ({ data, category }: { data: any, category: string }) => {
                             name={item.name}
                             singer={item.singer}
                             image={item.image}
+                            text={item.text}
                             path={item.path}
                         />
                     ))
@@ -64,35 +74,40 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
             name: "آمد بهار جان ها",
             path: "https://dls.music-fa.com/tagdl/downloads/Mohsen%20Chavoshi%20-%20Beraghsa%20(128).mp3",
             image: "https://music-fa.com/wp-content/uploads/2018/12/M-chavoshi4956439822146524268375268572682365.jpg",
-            singer: "محسن چاوشی"
+            singer: "محسن چاوشی",
+            text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
         },
         {
             id: 2,
             name: "شیک",
             path: "https://dls.music-fa.com/tagdl/downloads/Yousef%20Zamani%20-%20Shik%20(128).mp3",
             image: "https://music-fa.com/wp-content/uploads/2019/03/Y-zamani9856293865884752493.jpg",
-            singer: "یوسف زمانی"
+            singer: "یوسف زمانی",
+            text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
         },
         {
             id: 3,
             name: "بهت قول میدم",
             path: "https://dls.music-fa.com/tagdl/downloads/Mohsen%20Yeganeh%20-%20Behet%20Ghol%20Midam%20(128).mp3",
             image: "https://music-fa.com/wp-content/uploads/2018/12/M-yegane83658723567456837456986745867509673452355.jpg",
-            singer: "محسن یگانه"
+            singer: "محسن یگانه",
+            text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
         },
         {
             id: 4,
             name: "نشکن دلمو",
             path: "https://dls.music-fa.com/tagdl/downloads/Yegane%20Chavoshi%20Hakan%20-%20Nashkan%20Delamo%20(128).mp3",
             image: "https://music-fa.com/wp-content/uploads/2019/01/hakan-chavoshi-yegane9385239857243987524527.jpg",
-            singer: "محسن یگانه"
+            singer: "محسن یگانه",
+            text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
         },
         {
             id: 5,
             name: "آخرش قشنگه",
             path: "https://dls.music-fa.com/tagdl/downloads/Alireza%20Talischi%20-%20Akharesh%20Ghashange%20(128).mp3",
             image: "https://music-fa.com/wp-content/uploads/2018/10/A-talischi243264y235634.jpg",
-            singer: "علیرضا طلیسچی"
+            singer: "علیرضا طلیسچی",
+            text: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.'
         }
     ]
 
