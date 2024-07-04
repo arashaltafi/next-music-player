c9759a4ac3c390889122b18dfca9770c8f0517a7
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-04 20:24:40 +0430

    responsive

--- components/MusicVideoItem.tsx
+++ components/MusicVideoItem.tsx
@@ -13,7 +13,7 @@ const MusicVideoItem = (props: MusicVideoType) => {
 
     return (
         <div className={`w-full flex flex-col gap-4 items-center justify-between px-2 sm:px-4 md:px-6 lg:px-8 py-1 sm:py-2 md:py-3 lg:py-4 rounded-xl bg-slate-600 shadow-boxShadow ${props.isFav && 'bg-pattern-3 rounded-boxRadius'}`}>
-            <div className='mt-6 mb-8 w-full flex gap-16 items-center justify-between'>
+            <div className='mt-2 sm:mt-3 md:mt-4 lg:mt-6 mb-2 sm:mb-4 md:mb-6 lg:mb-8 w-full flex gap-16 items-center justify-between'>
                 <div className='w-full h-full flex flex-col gap-4 items-stretch justify-between'>
                     <h5 className='text-lg'>موزیک ویدیو: <span className='text-xl'>{props.name}</span></h5>
                     <h5 className='text-lg'>خواننده: <span className='text-xl'>{props.singer}</span></h5>
@@ -39,13 +39,13 @@ const MusicVideoItem = (props: MusicVideoType) => {
                     height={500}
                 />
             </div>
-            <div className='my-4 w-full flex items-center justify-end gap-8'>
+            <div className='my-1 sm:my-2 md:my-3 lg:my-4 w-full flex items-center justify-end gap-2 sm:gap-4 md:gap-6 lg:gap-8'>
                 <button
                     className='flex gap-2 items-center justify-center btn btn-sky-outline'
                     data-click={props.name}
                 >
                     ادامه مطلب
-                    <IoIosArrowBack />
+                    <IoIosArrowBack className='hidden sm:block' />
                 </button>
             </div>
         </div>
