9ab5fd4a11bd0607b7a9e5a6a6454f8475658868
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-05 16:05:22 +0430

    responsive

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -22,18 +22,18 @@ const Category = (props: PropsType) => {
                 <title>خواننده: {props.data.category} | موزیک آنلاین</title>
                 <meta name="description" content={`دسته بندی ${props.data.category} موزیک آنلاین`} />
             </Head>
-            <div className='mt-8 w-full flex flex-col gap-8 items-center justify-start px-8'>
+            <div className='mt-8 w-full flex flex-col gap-4 sm:gap-6 md:gap-8 items-center justify-start px-2 sm:px-4 md:px-6 lg:px-8'>
                 <SingerImage name='arash' src='https://arashaltafi.ir//melodyo/media/app/cat_pop.jpg' />
-                <h3 className='self-start font-bold text-4xl'>دسته بندی:</h3>
-                <h2 className='self-start font-medium text-xl'>{props.data.category}</h2>
+                <h3 className='self-start font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>دسته بندی:</h3>
+                <h2 className='mb-8 self-start font-medium text-sm sm:text-base md:text-lg lg:text-xl'>{props.data.category}</h2>
 
                 <HeadOfTitle fileType='music' route={RoutesAddress.MUSIC_ALL} title="موزیک ها:" />
-                <MusicsComponent category={MusicCategory.ALL} />
+                <MusicsComponent category={MusicCategory.ALL} className='mt-0' />
 
-                <DividerImage src='/images/music-divider-1.png' className='my-20' />
+                <DividerImage src='/images/music-divider-1.png' className='my-12 sm:my-16 md:my-20' />
 
                 <HeadOfTitle fileType='video' route={RoutesAddress.MUSIC_VIDEO_ALL} title="موزیک ویدیوها:" />
-                <MusicVideosComponent category={MusicVideoCategory.ALL} />
+                <MusicVideosComponent category={MusicVideoCategory.ALL} className='mt-0' />
             </div>
         </>
     )
