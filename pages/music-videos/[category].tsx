9ba5b3f452c7f3b62b4a6dcad6648745c79f51b9
Arash Altafi <arashaltafi1377@gmail.com>
2024-07-05 12:00:25 +0430

    responsive

--- pages/music-videos/[category].tsx
+++ pages/music-videos/[category].tsx
@@ -45,8 +45,8 @@ const MusicVideos = ({ data, category }: { data: MusicVideoType[], category: str
                 <title>{category} | موزیک آنلاین</title>
                 <meta name="description" content={`${category} موزیک آنلاین`} />
             </Head>
-            <div onClick={(e) => handleClick(e)} className='-mb-52 mt-10 w-full flex flex-col gap-12 items-center justify-start px-2 sm:px-4 md:px-6 lg:px-8'>
-                <h2 className='self-start font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl'>{category}:</h2>
+            <div onClick={(e) => handleClick(e)} className='-mb-52 mt-10 w-full flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-12 items-center justify-start px-2 sm:px-4 md:px-6 lg:px-8'>
+                <h2 className='self-start font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl my-4'>{category}:</h2>
 
                 {
                     category === 'موزیک ویدیوهای مورد علاقه' ? (
